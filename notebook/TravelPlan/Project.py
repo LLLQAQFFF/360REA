53199ee3aac230dcb8bf281ba79a97a5acb51204
Hao Li <100356158+LLLQAQFFF@users.noreply.github.com>
2024-05-19 23:39:43 +0800

    Add files via upload

---
+++ notebook/TravelPlan/Project.py
@@ -0,0 +1,19 @@
+class Project:
+    def __init__(self, name, goals, guidance):
+        self.name = name
+        self.goals = goals
+        self.guidance = guidance
+        self.stages = []
+        self.progress = []
+
+    def add_stage(self, stage):
+        # add a project stage
+        pass
+
+    def update_progress(self, update):
+        # update overall project progress
+        pass
+
+    def generate_project_summary(self):
+        # generate a project summary
+        pass
